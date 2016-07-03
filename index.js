189056c3b0e45db56dcc4d5deda36b958ec2926a
Parth Tiwari <parthtiwari65@gmail.com>
2016-07-02 20:30:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,18 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  },
+  push: {
+		android: {
+			senderId: '', // The Sender ID of GCM
+			apiKey: '' // The Server API Key of GCM
+		},
+		ios: {
+			pdx: 'certs/mycert.p12', // the path and filename to the .p12 file you exported earlier. 
+			bundleId: process.env.BUNDLE_ID || '', // The bundle identifier associated with your app
+			production: true
+		}
+	}
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
